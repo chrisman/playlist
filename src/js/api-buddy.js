db6d8044e873cc9668b2a17d795b03a9ac958a70
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-09 13:20:36 -0700

    js wired up

---
+++ src/js/api-buddy.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getObject: function(){
+    return {}
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var apibuddy = require('./api-buddy')
+var $ = require('jquery')
+
+$(document).ready(function(){
+  console.log('HELLLLO');
+})
