2a0ddaf2b0576d8ed2fe38234ecb26d1553bb13d
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-08 17:33:39 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var sass = require('gulp-sass')
+
+gulp.task('html', function(){
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('styles', function(){
+  return gulp.src('src/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('scripts', function(){
+  return browserify('src/js/main.js')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('watchman', function(){
+  gulp.watch('src/js/*js', ['scripts'])
+  gulp.watch('src/sass/*scss', ['styles'])
+  gulp.watch('src/index.html' ['html'])
+});
+
+gulp.task('default',['html', 'scripts', 'styles', 'watchman']);
